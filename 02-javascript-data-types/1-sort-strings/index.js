7b6fd13f82686653a8d85a8570a9474bf70ed7bd
klarksergeev <senen_ichanov@mail.ru>
2024-09-21 18:16:59 +0300

    Сделано первое задание второго занятия

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -1,9 +1,28 @@
+
 /**
  * sortStrings - sorts array of string by two criteria "asc" or "desc"
- * @param {string[]} arr - the array of strings
+ * @param {string[]} in_arr - the array of strings
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
 
+export function sortStrings(inArr, param = 'asc') {
+
+    let outArr = inArr.map((a)=>a);
+    let compareStrings = function(str1,str2){
+
+        let cmpReslt = str1.localeCompare(str2);
+        if(str1.toLowerCase().localeCompare(str2.toLowerCase()) != cmpReslt){
+            return ~cmpReslt;
+        } else {
+            return cmpReslt;
+        } 
+    }
+
+    if (param == 'asc'){
+        outArr.sort(compareStrings);
+    } else if (param == 'desc'){
+        outArr.sort((a,b) => ~compareStrings(a,b));
+    }
+    return outArr;
 }
