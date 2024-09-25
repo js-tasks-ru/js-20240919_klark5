b8c416db24622f59a0d3a190a5d43050071b3314
klarksergeev <senen_ichanov@mail.ru>
2024-09-25 23:21:13 +0300

    Сделал третье задание третьего занятия

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -6,4 +6,29 @@
  */
 export function trimSymbols(string, size) {
 
+    if (typeof string != 'string') return;
+    if (typeof size != 'number') return string;
+    if (size == 0) return '';
+
+    let newString = '';
+    let prevLetter = string[0];
+    let letterCounter = 0;
+
+    for( const current_letter of string ){
+
+        if(prevLetter == current_letter && letterCounter < size){
+
+            newString += current_letter;
+            letterCounter++;
+
+        } else if (prevLetter != current_letter) {
+
+            newString += current_letter;
+            prevLetter = current_letter;
+            letterCounter = 1;
+        }
+    }
+
+    return newString;
 }
+
